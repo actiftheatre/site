c40b968c9351b28bd5ca5c677d6fdeaff9b82989
lecepe <florian.delgado@gmail.com>
2019-12-26 16:25:52 +0100

    first

---
+++ js/main.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+    $('.parallax').parallax({
+      responsiveThreshold: 200,
+    });
+  });
+
+  $(document).ready(function(){
+    $('.slider').slider({
+      indicators: true,
+    });
+  });
+
+  $('.carousel.carousel-slider').carousel({
+    fullWidth: true,
+    indicators: true,
+  });
+
+  $(document).ready(function(){
+    $('.scrollspy').scrollSpy();
+  });
+
+  $(document).ready(function() {
+    M.updateTextFields();
+  });
+
+  $(document).ready(function(){
+    $('.sidenav').sidenav();
+  });
+        
+  $(document).ready(function(){
+    $('.modal').modal({
+      opacity: 0.8,
+      preventScrolling: false,
+    });
+  });
+
+  var toastEVI = '<span class="center"><b>Expérience de Vie Imminente</b> <br> Représentations prochainement!</span><a href="evi.html" class="btn-flat toast-action">RÉSERVER</a>';
