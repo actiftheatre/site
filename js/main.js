521e0cd6de0be51e98510dd4089717842c3fa97f
lecepe <florian.delgado@gmail.com>
2020-01-31 13:09:01 +0100

    mise à jour dossier et formulaire contact

--- js/main.js
+++ js/main.js
@@ -33,6 +33,30 @@ $(document).ready(function(){
       preventScrolling: false,
     });
   });
- 
+
+  $(document).ready(function() {
+		if($('#contact_form').length>0){
+			$('#contact_form')[0].reset(); 
+		}
+  });
+	
+$().ready(function() {
+  $("#contact_form").validate({
+  rules : {
+    first_name : {
+      required : true
+    },
+    last_name : {
+      required : true
+    },
+    email : {
+      required : true
+    },
+    message : {
+      required : true
+    },
+  },
+ });
+});
 
   var toastEVI = '<span class="center"><b>Expérience de Vie Imminente</b> <br> Représentations prochainement!</span><a href="evi.html" class="btn-flat toast-action">RÉSERVER</a>';
